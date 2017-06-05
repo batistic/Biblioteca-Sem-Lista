83a26f5c18188fc170be51198347c3e77ad1ebe3
Guilherme Atihe de Oliveira <g.atihe@Guilhermes-MacBook-Pro.local>
2017-06-05 01:33:06 -0300

    Cliente.h

--- Cliente.h
+++ Cliente.h
@@ -4,7 +4,30 @@
 class Cliente
 {
 	public:
+
+		Cliente();
+				~Cliente();
+				int editar_cadastro();
+				int get_CPF();
+				string get_Nome();
+
 	protected:
+				int CPF;
+				string nome;
+				char sexo; // [H] ou [M]
+				int data_nasc[3]; // {dd,mm,aaaa}
+				string endereco;
+				int CEP;
+				int telefone;
+				string email;
+				bool pendencia;
+				bool multa;
+				int limite_emp;
+				int prazo_padrao;
+
+
+
+
 };
 
 #endif
