8569387f2e8dce0286b6d6b83088fb7683bff833
Gustavo Batistic Ribeiro <gustavo.batistic@gmail.com>
2017-06-06 21:38:37 -0300

    editar cliente ta igual editar livro e ainda nao funfa

--- Cliente.cpp
+++ Cliente.cpp
@@ -68,7 +68,7 @@ void Cliente::mostrar_dados(){
   std::cout << "CPF: " << CPF << '\n';
   std::cout << "Nome: " << nome << '\n';
   std::cout << "Sexo: " << sexo << '\n';
-  std::cout << "4. Data de nascimento: " << data_nasc[0] << "/" << data_nasc[1] << "/" << data_nasc[2] << '\n';
+  std::cout << "Data de nascimento: " << data_nasc[0] << "/" << data_nasc[1] << "/" << data_nasc[2] << '\n';
   std::cout << "CEP: " << CEP << '\n';
   std::cout << "Endereco: " << endereco << '\n';
   std::cout << "Telefone: " << telefone << '\n';
@@ -77,6 +77,53 @@ void Cliente::mostrar_dados(){
 }
 
 int Cliente::editar_cadastro(){
+    int opc=-1;
+	cout << "1. CPF: " << CPF << '\n';
+	cout << "2. Nome: " << nome << '\n';
+	cout << "3. Sexo: " << sexo << '\n';
+	cout << "4. Data de nascimento: " << data_nasc[0] << "/" << data_nasc[1] << "/" << data_nasc[2] << '\n';
+	cout << "5. CEP: " << CEP << '\n';
+	cout << "6. Endereco: " << endereco << '\n';
+	cout << "7. Telefone: " << telefone << '\n';
+	cout << "8. Email: " << email << '\n';
+	while(opc != 0){
+		cout << "Insira o numero referente a informacao que deseja editar ou digite 0 para Sair"<<endl<<": ";
+  		cin >> opc;
+  		switch(opc){
+  			case 1: std::cout << "CPF: ";
+                    cin>>CPF;
+			  		break;
+			case 2: std::cout << "Nome: ";
+                    setbuf(stdin,NULL);
+                    std::getline(cin,nome);
+                    setbuf(stdin,NULL);
+			  		break;
+			case 3: std::cout << "Sexo (H | M): ";
+                    cin>>sexo;
+			  		break;
+			case 4: std::cout << "Dia de nascimento: ";
+                    std::cin >> data_nasc[0];
+                    std::cout << "Mes de nascimento: ";
+                    std::cin >> data_nasc[1];
+                    std::cout << "Ano de nascimento: ";
+                    std::cin >> data_nasc[2];
+			  		break;
+			case 5: std::cout << "CEP: ";
+                    std::cin >> CEP;
+			  		break;
+			case 6: std::cout << "Endereco: ";
+                    setbuf(stdin,NULL);
+                    std::getline(cin,endereco);
+                    setbuf(stdin,NULL);
+			  		break;
+			case 7: std::cout << "Email: ";
+                    setbuf(stdin,NULL);
+                    std::getline(cin,email);
+                    setbuf(stdin,NULL);
+			  		break;
+		}
+	}
+/*
   int resposta = 9;
   string n;
   std::cout << "1. CPF: " << CPF << '\n';
@@ -152,6 +199,7 @@ int Cliente::editar_cadastro(){
     std::cin >> resposta;
   }
   return 0;
+ */
 }
 
 void Cliente::adicionarEmprestimo(){
